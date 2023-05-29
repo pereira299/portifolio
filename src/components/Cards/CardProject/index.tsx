6067a171689e8262e543a1a6a11ceb3fa3f39029
pereira299 <lucasper057@gmail.com>
2023-05-29 17:49:37 -0300

    projects

--- src/components/Cards/CardProject/index.tsx
+++ src/components/Cards/CardProject/index.tsx
@@ -1,4 +1,5 @@
 import Chip from "@/components/Chip";
+import { Link } from "lucide-react";
 import Image from "next/image";
 
 interface CardProjectProps {
@@ -13,20 +14,21 @@ interface CardProjectProps {
 
 export default function CardProject(props: CardProjectProps) {
   const { title, image, description, link, stack } = props.project;
+  const shortText = description.slice(0, 80) + " ...";
   return (
     <article className="w-3/12 preserve-3d transition-all h-full hover:-rotate-y-180 duration-300 relative shadow">
-      <div className="bg-gray-700 rounded-md p-3 h-fit w-full gap-y-2 grid face-hidden absolute -mt-20">
+      <div className="bg-gray-700 rounded-md border-t-8 border-sky-500 p-3 h-fit w-full gap-y-2 grid face-hidden absolute -mt-20">
         <Image
-          src={image}
+          src={`/images/projects/${image}.png`}
           alt={`Site ${title}`}
           width={400}
           height={300}
           className="rounded-md w-full h-auto"
         />
         <h5 className="text-white font-bold text-md">{title}</h5>
-        <p className="text-white text-sm">{description}</p>
+        <p className="text-white text-sm">{shortText}</p>
       </div>
-      <div className="face-hidden bg-gray-700 p-3 h-max w-full rounded-md rotate-y-180 absolute -mt-20">
+      <div className="face-hidden bg-gray-700 border-t-8 border-sky-500 p-3 h-max w-full rounded-md rotate-y-180 absolute -mt-20">
         <h5 className="text-white font-bold text-md">{title}</h5>
         <p className="text-white text-sm">{description}</p>
         <div className="flex flex-row gap-x-2 mt-2 flex-wrap gap-y-2">
@@ -34,6 +36,12 @@ export default function CardProject(props: CardProjectProps) {
             <Chip key={item} text={item} />
           ))}
         </div>
+        <Link
+          href={link}
+          className="border-sky-600 border-2 w-full rounded-md px-2 py-1 mt-2 hover:border-white hover:text-white"
+        >
+          <p className="text-sky-600">Acessar o site</p>
+        </Link>
       </div>
     </article>
   );
--- src/components/Sections/Projects/index.tsx
+++ src/components/Sections/Projects/index.tsx
@@ -1,37 +1,15 @@
 import CardProject from "@/components/Cards/CardProject";
 import TitleSection from "@/components/TitleSection";
+import projects from "../../../../public/projects.json"
 
 export default function Projects() {
-    const projects = [
-        {
-            title: "Projeto 1",
-            description: "Descrição do projeto 1",
-            image: "https://via.placeholder.com/600x400",
-            link: "https://google.com",
-            stack: ["React", "TailwindCSS", "Next.JS", "TypeScript"]
-        },
-        {
-            title: "Projeto 2",
-            description: "Descrição do projeto 2",
-            image: "https://via.placeholder.com/600x400",
-            link: "https://google.com",
-            stack: ["React", "TailwindCSS", "Next.JS", "TypeScript"]
-        },
-        {
-            title: "Projeto 3",
-            description: "Descrição do projeto 3",
-            image: "https://via.placeholder.com/600x400",
-            link: "https://google.com",
-            stack: ["React", "TailwindCSS", "Next.JS", "TypeScript"]
-        },
-    ]
-
+    const vitrine = projects.filter((project) => project.vitrine === true)
 
     return (
         <section id="projects" className="h-[90vh] flex flex-col items-center py-10">
             <TitleSection title="Projetos" className="mx-auto" />
             <div className="flex flex-row justify-between w-8/12 my-auto">
-                {projects.map((project) => (
+                {vitrine.map((project) => (
                     <CardProject project={project} key={project.title} />
                 ))}
             </div>
