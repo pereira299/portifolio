57df76b659e1b656fede4dc1e89bc5efdf320240
pereira299 <lucasper057@gmail.com>
2023-05-30 11:50:51 -0300

    fix card flip bug

--- src/components/Cards/CardProject/index.tsx
+++ src/components/Cards/CardProject/index.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Chip from "@/components/Chip";
-import { Link } from "lucide-react";
+import { gsap } from "gsap";
 import Image from "next/image";
+import Link from "next/link";
+import { MouseEvent, useLayoutEffect, useRef, useState } from "react";
 
 interface CardProjectProps {
   project: {
@@ -13,36 +16,80 @@ interface CardProjectProps {
 }
 
 export default function CardProject(props: CardProjectProps) {
+  const [tl, setTl] = useState<gsap.core.Timeline>();
   const { title, image, description, link, stack } = props.project;
-  const shortText = description.slice(0, 80) + " ...";
+  const front = useRef<HTMLDivElement>(null);
+  const back = useRef<HTMLDivElement>(null);
+  const card = useRef<HTMLDivElement>(null);
+  const count = description.split(" ").reduce((acc, item) => {
+    if (acc < 90) {
+      acc += item.length + 1;
+    }
+    return acc;
+  }, 0);
+  const shortText = description.slice(0, count) + "...";
+
+  useLayoutEffect(() => {
+    setTl(
+      gsap
+        .timeline({ paused: true })
+        .to(front.current, { duration: 1, rotationY: 180 })
+        .to(back.current, { duration: 1, rotationY: 0 }, 0)
+        .to(card.current, { z: 50 }, 0)
+        .to(card.current, { z: 0 }, 0.5)
+    );
+  }, []);
+
+  const play = (e: MouseEvent<HTMLDivElement>) => {
+    if (tl) tl.play();
+  };
+
+  const reverse = (e: MouseEvent<HTMLDivElement>) => {
+    if (tl) tl.reverse();
+  };
   return (
-    <article className="w-3/12 preserve-3d transition-all h-full hover:-rotate-y-180 duration-300 relative shadow">
-      <div className="bg-gray-700 rounded-md border-t-8 border-sky-500 p-3 h-fit w-full gap-y-2 grid face-hidden absolute -mt-20">
-        <Image
-          src={`/images/projects/${image}.png`}
-          alt={`Site ${title}`}
-          width={400}
-          height={300}
-          className="rounded-md w-full h-auto"
-        />
-        <h5 className="text-white font-bold text-md">{title}</h5>
-        <p className="text-white text-sm">{shortText}</p>
-      </div>
-      <div className="face-hidden bg-gray-700 border-t-8 border-sky-500 p-3 h-max w-full rounded-md rotate-y-180 absolute -mt-20">
-        <h5 className="text-white font-bold text-md">{title}</h5>
-        <p className="text-white text-sm">{description}</p>
-        <div className="flex flex-row gap-x-2 mt-2 flex-wrap gap-y-2">
-          {stack.map((item) => (
-            <Chip key={item} text={item} />
-          ))}
-        </div>
-        <Link
-          href={link}
-          className="border-sky-600 border-2 w-full rounded-md px-2 py-1 mt-2 hover:border-white hover:text-white"
+    <>
+      <div id="mainWrap" onMouseEnter={play} onMouseLeave={reverse}>
+        <div
+          className="w-72 h-80 float-left mr-3 relative preserve-3d"
+          ref={card}
         >
-          <p className="text-sky-600">Acessar o site</p>
-        </Link>
+          <div
+            className="rounded-lg absolute w-full h-full bg-gray-700 border-t-8 border-sky-500 p-3 face-hidden"
+            ref={front}
+          >
+            <Image
+              src={`/images/projects/${image}.png`}
+              alt={`Site ${title}`}
+              width={400}
+              height={300}
+              className="rounded-md w-full h-auto"
+            />
+            <h5 className="text-white font-bold text-md mt-5 mb-3">{title}</h5>
+            <p className="text-white text-sm">{shortText}</p>
+          </div>
+          <div
+            className="rounded-lg absolute w-full flex flex-col justify-between h-full bg-gray-700 border-t-8 border-sky-500 p-3 face-hidden -rotate-y-180"
+            ref={back}
+          >
+            <h5 className="text-white font-bold text-md">{title}</h5>
+            <p className="text-white text-sm">{description}</p>
+            <div className="flex flex-row gap-x-2 mt-2 flex-wrap gap-y-2">
+              {stack.map((item) => (
+                <Chip key={item} text={item} />
+              ))}
+            </div>
+            <Link
+              href={link}
+              className="border-sky-600 flex border-2 w-full rounded-md px-2 py-1 hover:border-white hover:text-white text-sky-600"
+            >
+              <p className=" text-center w-full font-semibold">
+                Acessar o site
+              </p>
+            </Link>
+          </div>
+        </div>
       </div>
-    </article>
+    </>
   );
 }
